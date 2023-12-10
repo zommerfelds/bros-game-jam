921e7ccadd1eac4f3b3bfc77c861bd309696f658
Christian Zommerfelds <zommerfelds@users.noreply.github.com>
2023-12-10 21:13:11 +0000

    Load tiles in a hard coded map

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,50 @@
 import 'phaser';
 
 class MyGame extends Phaser.Scene {
+
+    readonly TILES_BOX1 = 0;
+    readonly TILES_BOX2 = 1;
+    readonly TILES_WALL_DIRT = 2;
+    readonly TILES_FLOOR_SAND = 8;
+    readonly TILES_FLOOR_DIRT = 9;
+
     constructor() {
         super();
     }
 
     preload() {
         this.load.path = 'assets/';
-        this.load.image('platform', 'Platform.png');
-        this.load.aseprite('swordGuy', 'Sword guy.png', 'Sword guy.json');
+        this.load.spritesheet('tiles', 'tileset.png', { frameWidth: 16, frameHeight: 16 });
     }
 
     create() {
-        this.anims.createFromAseprite('swordGuy');
-        this.add.image(64, 66, 'platform');
-        const swordGuy = this.add.sprite(88, 50, 'swordGuy').play({ key: 'walk', repeat: -1 });
 
-        this.tweens.chain({
-            targets: swordGuy,
-            tweens: [
-                {
-                    x: 40,
-                    duration: 1500,
-                    onComplete: () => { swordGuy.flipX = true; },
-                },
-                {
-                    x: 88,
-                    duration: 1500,
-                    onComplete: () => { swordGuy.flipX = false; },
+        const map = [
+            [2, 2, 2, 2, 2, 2, 2, 2],
+            [2, 8, 8, 8, 0, 8, 8, 2],
+            [2, 2, 8, 8, 8, 8, 8, 2],
+            [-1, 2, 2, 2, 2, 8, 2, 2],
+            [-1, -1, -1, -1, 2, 8, 2, -1],
+            [-1, 2, 2, 2, 2, 8, 2, -1],
+            [-1, 2, 9, 9, 9, 9, 2, -1],
+            [-1, 2, 9, 9, 9, 9, 2, -1],
+            [-1, 2, 9, 0, 9, 9, 2, -1],
+            [-1, 2, 9, 9, 9, 9, 2, -1],
+            [-1, 2, 9, 9, 1, 9, 2, -1],
+            [-1, 2, 9, 9, 9, 9, 2, -1],
+            [-1, 2, 2, 2, 2, 2, 2, -1],
+        ];
+        const mapWidth = map[0].length;
+        const mapHeight = map.length;
+
+        for (let y = 0; y < mapHeight; y++) {
+            for (let x = 0; x < mapWidth; x++) {
+                const tileId = map[y][x];
+                if (tileId != -1) {
+                    this.add.image(10 + 8 + 16 * x, 10 + 8 + 11 * y, 'tiles', tileId);
                 }
-            ],
-            loop: -1
-        });
-
+            }
+        }
     }
 }
 
@@ -42,8 +54,8 @@ const config = {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
-    width: 128,
-    height: 96,
+    width: 148,
+    height: 220,
     zoom: 5,
     pixelArt: true,
     antialias: false,
